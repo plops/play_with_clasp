cae495aafb97d512583ee02486eaef91d669d2d9
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-11-01 00:17:16 +0100

    some code

---
+++ 1.lisp
@@ -0,0 +1,25 @@
+(ql:quickload "cffi")
+
+(defun sub (a b)
+  (- a b))
+
+(defpackage #:g
+  (:use #:cl #:core #:clasp-ffi))
+(in-package #:g)
+
+(list
+ (char-code #\M)
+ (cffi:foreign-funcall "toupper"
+		       :char
+		       (char-code #\m)
+		       :char))
+
+
+(cffi:with-foreign-pointer-as-string
+    (s 100)
+  (setf (cffi:mem-ref s :char) 0)
+  (cffi:foreign-funcall "sprintf"
+			:pointer s
+			:string "%d"
+			:int 1000
+			:int))
